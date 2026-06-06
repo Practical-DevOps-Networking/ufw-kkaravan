2ca89757d5cfd6f1850ae5e8753bccf4061291aa
Kostia Karavan <kkaravan008@gmail.com>
2026-06-06 19:02:38 +0300

    Fix: Change 'deny' to 'reject' for port 3005 rule to match workflow expectations

--- add-rules.sh
+++ add-rules.sh
@@ -10,7 +10,7 @@ OUTPUT_FILE=$1
 sudo ufw allow from 127.0.0.1 to any port 3306 proto tcp
 
 sudo ufw allow from 192.168.32.55 to any port 3005 proto tcp
-sudo ufw deny to any port 3005 proto tcp
+sudo ufw reject to any port 3005 proto tcp
 
 sudo ufw allow in on eth0 to any port 8099
 
@@ -19,4 +19,3 @@ sudo ufw limit 6050:6055/tcp
 sudo ufw --force enable
 
 sudo ufw status numbered > "$OUTPUT_FILE"
-
